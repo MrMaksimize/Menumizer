eed194303b07f8e1ab4241bd729636109587e8c8
Maksim Pecherskiy <max@maksimize.com>
2012-04-07 08:45:33 -0500

    base operations now working, and lib is usable in simplest form

--- menumizer.js
+++ menumizer.js
@@ -1,22 +1,4 @@
-/*!
-* jQuery lightweight plugin boilerplate
-* Original author: @ajpiano
-* Further changes, comments: @addyosmani
-* Licensed under the MIT license
-*/
-
-// the semi-colon before the function invocation is a safety
-// net against concatenated scripts and/or other plugins
-// that are not closed properly.
 ;(function ( $, window, document, undefined ) {
-
-  // undefined is used here as the undefined global
-  // variable in ECMAScript 3 and is mutable (i.e. it can
-  // be changed by someone else). undefined isn't really
-  // being passed in so we can ensure that its value is
-  // truly undefined. In ES5, undefined can no longer be
-  // modified.
-
   // window and document are passed through as local
   // variables rather than as globals, because this (slightly)
   // quickens the resolution process and can be more
@@ -37,7 +19,7 @@
     };
 
   // The actual plugin constructor
-  function Menumizer( element, options ) { 
+  function Menumizer( element, options ) {
     // jQuery has an extend method that merges the
     // contents of two or more objects, storing the
     // result in the first object. The first object
@@ -51,28 +33,27 @@
     this.state = '';
     this.prevState = '';
     this.stateController = this.options.stateController;
-    // Define properties for managing menus
-    console.log(this);
-    this.menuToDropDown = function(menu){
-      // need to know: menu top container, menu ul class, 
-      // get top parent container
-      var topContainer = $(menu).parent();
-      $(topContainer).append('<select/>');
-      var menuItems = $(topContainer).find('li');
+    //this.processedMenus = {};
+
+    this.menuToDropDown = function(singleMenu){
+      var singleMenuParent = singleMenu.parentElement || $(singleMenu).parent();
+      $(singleMenuParent).append('<select class="menumizer-select"/>');
+      var menuItems = $(singleMenuParent).find('li');
       $(menuItems).each(function() {
         var optionString = '<option value = "' + $('a', this).attr('href') + '" ';
           if($(this).hasClass('active')){
             optionString = optionString + 'selected="selected" '
           }
         optionString = optionString + '>' + $('a', this).text() + '</a>';
-        $('select', topContainer).append(optionString); // @todo performance by compiling string and appending all at once
+        // @todo - deal with multiple selects
+        $('select', singleMenuParent).append(optionString); // @todo performance by compiling string and appending all at once
       });
-
-      $(menu).remove();
+      $(singleMenu).addClass('menumizer-hidden').hide();
     };
 
     this.dropDownToMenu = function () {
-
+      $('.menumizer-select').remove();
+      $('.menumizer-hidden').removeClass('menumizer-hidden').show();
     };
 
     this.getMenus = function () {
@@ -81,20 +62,15 @@
       var menus = this.options.menus;
       if ($.isEmptyObject(menus)){
         menus = $(this.element).find('ul');
-        console.log(menus);
         return menus;
       }
       else {
         for (var menu in this.menus){
-          console.log(menu); // untested
+          alert('freakout'); // untested
+          return menu;
         }
-      } 
+      }
     };
-
-    this.getState = function () {
-    
-    };
-
     this.init();
   }
 
@@ -111,6 +87,7 @@
     // Alias this to base
     var base = this;
     if (base.state == true) {
+      console.log('menumize');
       $(base.menus).each(function(index, singleMenu){
         base.menuToDropDown(singleMenu);
       });
@@ -121,24 +98,22 @@
       // Determine current state
       base.state = base.stateController();
       console.log($(window).width());
-      //console.log('state ' + base.state);
-      //console.log('prevState ' + base.prevState);
       if (base.prevState != base.state){
-        // We know SOMETHING needs to happen here.  But what?
-          if (base.state == false) {
-            // UnMenumize
-            console.log('unmenumize');
-
-          }
-          else {
-            // MenuMize
-            console.log('menumize');
-            $(base.menus).each(function(index, singleMenu) {
-              base.menuToDropDown(singleMenu);
-            });
-         } 
+        if (base.state == false) {
+          // UnMenumize
+          console.log('unmenumize');
+          base.dropDownToMenu();
+        }
+        else {
+          // MenuMize
+          console.log('menumize');
+          $(base.menus).each(function(index, singleMenu) {
+            base.menuToDropDown(singleMenu);
+          });
+         }
         //end by making state prevState
         base.prevState = base.state;
+        console.log(base.processedMenus);
       }
     });
   };
