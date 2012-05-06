9274a8f39d03b35299741b61020a3ee6ea2cf96f
Maksim Pecherskiy <max@maksimize.com>
2012-05-06 07:23:50 -0500

    some cleanup and default statecontrol

--- menumizer.js
+++ menumizer.js
@@ -1,28 +1,19 @@
 ;(function ( $, window, document, undefined ) {
-  // window and document are passed through as local
-  // variables rather than as globals, because this (slightly)
-  // quickens the resolution process and can be more
-  // efficiently minified (especially when both are
-  // regularly referenced in your plugin).
-
-  // Create the defaults once
-  // need to know - top container, ul class
-  // can get top container, but ul classes are needed.
   var menumize = 'defaultMenumize',
     defaults = {
       // ul selectors for menus
-      menus: {}, // menus that will be affected
-      stateController: {}
-        // must return TRUE for minimize, false for maximize
+      // menus that will be affected
+      // contains smush and menumize objects
+      menus: {},
+      stateController: function() {
+        var width = $(window).width();
+        return width <= 500;
+      }
+      // must return TRUE for minimize, false for maximize
     };
 
   // The actual plugin constructor
   function Menumizer( element, options ) {
-    // jQuery has an extend method that merges the
-    // contents of two or more objects, storing the
-    // result in the first object. The first object
-    // is generally empty because we don't want to alter
-    // the default options for future instances of the plugin
     this.options = $.extend( {}, defaults, options) ;
     this.element = element;
     this._defaults = defaults;
@@ -88,37 +79,13 @@
           menus.menumize[index] = $(singleMenu, this.element);
         });
       }
-      /*else {
-        // handle simple menumizing
-        if (!$.isEmptyObject(menus.menumize)) {
-          //var counter = 0;
-          //var simpleMenumize = {};
-          //for (var i in menus.menumize) {
-            //$(menus.menumize[i]).each(function(index, Element) {
-              //simpleMenumize[counter] = Element;
-              //counter++;
-            //});
-          //}
-          console.log(menus.menumize);
-          //menus.menumize = simpleMenumize;
-        }
-        // Handle smushing.
-        if (!$.isEmptyObject(menus.smush)) {
-          for (var i in menus.smush) {
-            console.log('poop');
-          }
-        }
-      }*/
       return menus;
     };
+
     this.init();
   }
 
   Menumizer.prototype.init = function () {
-    // Place initialization logic here
-    // You already have access to the DOM element and
-    // the options via the instance, e.g. this.element
-    //) and this.options
     this.menus = this.getMenus();
     this.state = this.stateController();
     this.prevState = this.state;
@@ -128,10 +95,6 @@
     var base = this;
     if (base.state == true) {
       console.log('menumize');
-      //$(base.menus.menumize).each(function(index, singleMenu){
-        //console.log(singleMenu);
-        //base.menuToDropDown(singleMenu);
-      //});
       for (var i in base.menus.menumize) {
         base.menuToDropDown(base.menus.menumize[i]);
       }
@@ -154,19 +117,19 @@
         else {
           // MenuMize
           console.log('menumize');
-          $(base.menus.menumize).each(function(index, singleMenu) {
-            base.menuToDropDown(singleMenu);
-          });
+          for (var i in base.menus.menumize) {
+            base.menuToDropDown(base.menus.menumize[i]);
+          }
+          for (var i in base.menus.smush) {
+            base.menuToDropDown(base.menus.smush[i], true);
+          }
          }
         //end by making state prevState
         base.prevState = base.state;
-        console.log(base.processedMenus);
       }
     });
   };
 
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
   $.fn.menumize = function ( options ) {
     return this.each(function () {
       if (!$.data(this, 'plugin_' + menumize)) {
